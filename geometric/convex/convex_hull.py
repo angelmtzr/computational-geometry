5ba0e5ae4ae552eb44336ee15843fd92bc3dc7b9
angelmtzr <0235347@up.edu.mx>
2023-05-07 17:54:29 -0600

    Moved the display_convex_hull() function to the ConvexHull class.

--- geometric/convex/convex_hull.py
+++ geometric/convex/convex_hull.py
@@ -1,9 +1,25 @@
+import matplotlib.pyplot as plt
 from dataclasses import dataclass
 from typing import List
 from geometric import Point
+from .convex_hull_calculator import ConvexHullCalculator
 
 
-@dataclass(slots=True, frozen=True)
+@dataclass(slots=True)
 class ConvexHull:
     points: List[Point]
     vertices: List[Point]
+
+    def __init__(self, points: List[Point], calculator: ConvexHullCalculator):
+        self.points = points
+        self.vertices = calculator.calculate(points)
+
+    def display(self):
+        """Displays the convex hull using matplotlib."""
+        x, y = zip(*self.points)
+        plt.scatter(x, y)  # Draws the points
+        vertices = self.vertices.copy()
+        vertices.append(vertices[0])  # We need to add the first vertex at the end to close the convex hull
+        x, y = zip(*vertices)
+        plt.plot(x, y, c="orange")  # Draws the edges
+        plt.show()
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random
-import matplotlib.pyplot as plt
 from typing import List
 from geometric import Point
-from geometric.convex import ConvexHullCalculator, ConvexHull, SlowConvexHullCalculator
+from geometric.convex import ConvexHull, SlowConvexHullCalculator
 
 
 def generate_random_points(*, n: int = 10, lower_limit: int = -100,
@@ -14,25 +13,10 @@ def generate_random_points(*, n: int = 10, lower_limit: int = -100,
                   random.randint(lower_limit, upper_limit)) for _ in range(n)]
 
 
-def display_convex_hull(convex_hull: ConvexHull) -> None:
-    """Displays a convex hull."""
-    x, y = zip(*convex_hull.points)
-    plt.scatter(x, y)
-    vertices = convex_hull.vertices.copy()
-    vertices.append(vertices[0])  # We need to add the first vertex at the end to close the convex hull
-    x, y = zip(*vertices)
-    plt.plot(x, y, c="orange")  # Draws the edges
-    plt.show()
-
-
-def calculate_convex_hull(points: List[Point], calculator: ConvexHullCalculator) -> ConvexHull:
-    return ConvexHull(points, calculator.calculate(points))
-
-
 def main():
-    points = generate_random_points(seed=50771708)
-    convex_hull = calculate_convex_hull(points, SlowConvexHullCalculator())
-    display_convex_hull(convex_hull)
+    points = generate_random_points(n=50)
+    convex_hull = ConvexHull(points, SlowConvexHullCalculator())
+    convex_hull.display()
 
 
 if __name__ == "__main__":
